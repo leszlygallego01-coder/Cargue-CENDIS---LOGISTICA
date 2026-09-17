12b51423aaab4824a95000e672e848c151efd845
leszlygallego01-coder <318311026+leszlygallego01-coder@users.noreply.github.com>
2026-09-17 13:45:41 -0500

    Update API URL in CONFIG_DEFAULT

--- cargue_script.js
+++ cargue_script.js
@@ -269,7 +269,7 @@ function autocompletarRuta(inputDestinoId, inputRutaId) {
    1. CONFIGURACION POR DEFECTO
    ═════════════════════════════════════════════════════════════════════════════════ */
 var CONFIG_DEFAULT = {
-  api_url: 'https://script.google.com/macros/s/AKfycbzHlFnwYX_QJq_ECqUJwhgv3XLEu68a4Iw-4YON1c6sgcG-dEGzGx9u95yeY_bW-1zZ/exec',
+  api_url: 'https://script.google.com/macros/s/AKfycbzO3Kk4WG27NmWLcaXdjyZiH68wOv3J-XYvOZlakExS_AyV-g87ydymE5NHYvtvJJtb/exec',
   fileIds: {
     trasladosEntrega: '1tkV0zSCigfxxukJ_Khdl-BYkw3Ex3tcGpiCS8gnGe_o'
   },
